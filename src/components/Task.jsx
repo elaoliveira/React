ed10af9674015d6abbedff3119d5a1b9bed26f78
Rafaela <rafaela.o.barbosa@gmail.com>
2022-01-04 15:31:58 -0300

    Introd React

---
+++ src/components/Task.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./Task.css";
+import {CgClose, CgInfo} from 'react-icons/cg'
+
+
+const Task = ({task, handleTaskClick, handleTaskDeletion}) => {
+    return(
+        <div className="task-container" 
+        style={task.completed ? {borderLeft: '6px solid #7FFF00'} : {}}>
+            <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+                {task.title}
+            </div>
+
+            <div className="buttons-container">
+                <button
+                className="remove-task-button" 
+                onClick={()=>handleTaskDeletion(task.id)}
+                >
+                    <CgClose/>
+                </button>
+                <button
+                className="see-task-details-button"
+                >
+                    <CgInfo/>
+                </button>
+            </div>
+        </div>
+    );
+    
+    // return <div className="task-container">{task.title}</div>;
+};
+
+export default Task;
